1ade9a5eab32ec579ba1e6d9b96ef9187673ecc5
AnnCa44 <annouchki@hotmail.fr>
2020-04-29 11:57:34 +0200

    first commit

---
+++ src/main/java/com/starlove/starlove/StarloveApplication.java
@@ -0,0 +1,13 @@
+package com.starlove.starlove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarloveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarloveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/starlove/starlove/StarloveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starlove.starlove;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarloveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
