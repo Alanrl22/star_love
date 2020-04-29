d5c6cc5665d5e66de661a4f36c397d814b7c7b23
elisego <elise.godard@gmail.com>
2020-04-29 14:46:51 +0200

    creation controller

---
+++ src/main/java/com/starlove/starlove/controller/MainController.java
@@ -0,0 +1,17 @@
+package com.starlove.starlove.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/home")
+    public String home(){
+
+        return "home";
+    }
+
+}
