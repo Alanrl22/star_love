43661ac6643c7bd1332acd72078a08de7839a98c
Alanrl22 <alan.raoul22@gmail.com>
2020-04-29 15:21:38 +0200

    Create class profile and constructor & getter & setter

---
+++ src/main/java/com/starlove/starlove/entity/profile.java
@@ -0,0 +1,60 @@
+package com.starlove.starlove.entity;
+
+public class profile {
+
+    private int id;
+    private String genre;
+    private String name;
+    private String hair;
+    private String image;
+
+    public profile(int id, String genre, String name, String hair, String image) {
+        this.id = id;
+        this.genre = genre;
+        this.name = name;
+        this.hair = hair;
+        this.image = image;
+    }
+
+    public profile() {}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getHair() {
+        return hair;
+    }
+
+    public void setHair(String hair) {
+        this.hair = hair;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
